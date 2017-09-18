ae9dc00a54ae4c4520c07ba5fd01354c1d875dc8
Manohar Vanga <manohar.vanga@gmail.com>
2017-09-18 23:56:45 +0200

    Make the whole thing a module
    Signed-off-by: Manohar Vanga <manohar.vanga@gmail.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pybootstrap',
+  packages = ['pybootstrap'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Manohar Vanga',
+  author_email = 'manohar.vanga@gmail.com',
+  url = 'https://github.com/mvanga/pybootstrap', # use the URL to the github repo
+  download_url = 'https://github.com/mvanga/pybootstrap/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['statistics', 'bootstrap', 'confidence'], # arbitrary keywords
+  classifiers = [],
+)
